d8c770757f4b92414162b48890ab538a2c7be585
Tritam Moahnty <77680898+Tritam1801@users.noreply.github.com>
2021-11-26 23:12:50 -0800

    All project files

---
+++ app.py
@@ -0,0 +1,122 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
+from pathlib import Path
+from werkzeug.utils import secure_filename
+import pandas as pd, numpy as np, os
+
+INPUT_FOLDER = '/static'
+OUTPUT_FOLDER = '/static'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Att_Db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['INPUT_FOLDER'] = INPUT_FOLDER
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+db = SQLAlchemy(app)
+
+
+class Att_Db(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    total = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"{self.date} - {self.total}"
+
+
+@app.route("/")
+def home(): 
+    attendance= Att_Db.query.all()
+    return render_template("index.html", attendance= attendance)
+
+@app.route("/about")
+def about(): 
+    return render_template("about.html")
+
+@app.route("/results", methods= ["POST","GET"])
+def results():
+    if request.method == "POST":
+        course= request.form['semester']
+
+        # getting the minimum percentage
+        min_perc= int(request.form['percentage'])
+
+        # Reaing required files
+        inputFile = Path(request.form['inputFile']).absolute()
+        outputFile= Path(request.form['outputFile']).absolute()
+        csv_data = pd.read_csv(inputFile, skiprows=4)
+        output_xls_cs = pd.read_excel(outputFile, sheet_name= 'Sheet1')
+        output_xls_ds = pd.read_excel(outputFile, sheet_name= 'Sheet2')
+
+        print(inputFile, outputFile)
+
+        # Striping out the requied date
+        att_date = datetime.strptime(csv_data['First Seen'][0], '%Y-%m-%d %H:%M:%S').date()
+            
+        # Time calculation
+        time_minutes = []
+        for time in csv_data['Time in Call']:
+            delta = timedelta(hours= int(time.split(':')[0]), minutes= int(time.split(':')[1]), seconds= int(time.split(':')[2]))
+            minutes = delta.total_seconds()/60
+            time_minutes.append(minutes)
+        csv_data['Minutes Present'] = time_minutes
+            
+        # marking the present(1) or asbent(0) in csv dataframe.
+        csv_data['Present/Absent']= np.where(csv_data['Minutes Present'] >= (max(csv_data['Minutes Present'])*(min_perc/100)), 1, 0)
+
+        # Insering a blank column with the required date in output file
+        output_xls_cs[str(att_date)] = [np.NaN]*len(output_xls_cs)
+        output_xls_ds[str(att_date)] = [np.NaN]*len(output_xls_ds)
+            
+        # storing the appropriate attendance in output file
+        for i in range(len(output_xls_cs)):
+            for j in range(len(csv_data)):
+                if output_xls_cs.loc[i, 'Student Name'].casefold() == csv_data.loc[j, 'Full Name'].casefold():
+                    output_xls_cs.loc[i, str(att_date)] = csv_data.loc[j, 'Present/Absent'].astype(int)
+            
+        output_xls_cs[str(att_date)] = output_xls_cs[str(att_date)].fillna(0)
+        output_xls_cs[str(att_date)] = output_xls_cs[str(att_date)].astype(int)
+            
+        # storing the appropriate attendance in output file
+        for i in range(len(output_xls_ds)):
+            for j in range(len(csv_data)):
+                if output_xls_ds.loc[i, 'Student Name'].casefold() == csv_data.loc[j, 'Full Name'].casefold():
+                    output_xls_ds.loc[i, str(att_date)] = csv_data.loc[j, 'Present/Absent'].astype(int)
+            
+        output_xls_ds[str(att_date)] = output_xls_ds[str(att_date)].fillna(0)
+        output_xls_ds[str(att_date)] = output_xls_ds[str(att_date)].astype(int)
+            
+        old_count= csv_data['Present/Absent'].sum()
+        new_count= output_xls_ds[str(att_date)].sum()+output_xls_cs[str(att_date)].sum()+1
+            
+        print("Input Count:",old_count)
+        print("Output Count:",new_count)
+
+        total_count = new_count-1
+
+        att = Att_Db(course= course, date=att_date, total= str(total_count))
+        db.session.add(att)
+        db.session.commit()
+                       
+        writer = pd.ExcelWriter(outputFile, engine= 'xlsxwriter')
+            
+        # storing results in output file
+        output_xls_cs.to_excel(writer, sheet_name= 'Sheet1', index=False)
+        output_xls_ds.to_excel(writer, sheet_name= 'Sheet2', index=False)
+            
+        writer.save()
+        
+    return redirect('/')
+    
+
+@app.route("/refresh")
+def refresh():
+    db.session.query(Att_Db).delete()
+    db.session.commit()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug= True, port=8001)
